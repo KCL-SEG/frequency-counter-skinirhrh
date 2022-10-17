d9d5ab4d9e934f6c631753f325a7c3c9909c5618
Rian Haque <rian.haque@hotmail.co.uk>
2022-10-17 12:21:03 +0100

    Done

--- frequencies.py
+++ frequencies.py
@@ -4,5 +4,6 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
-    print ("Hello")
-    return frequencies
+    for item in items:
+        frequencies[str(item)] = frequencies.get(str(item),0 ) + 1
+    return (frequencies)
